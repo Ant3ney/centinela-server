65101a5dd03eb108b26004a8cc79006de5b87fee
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2022-04-17 13:31:57 -0700

    Add corse

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const { google } = require('googleapis');
 const app = express();
 const eventsRoutes = require('./routes/events');
 require('dotenv').config();
+let cors = require('cors');
+
+let whitelist = ['', 'http://localhost:3000', 'https://centinelabible.org', 'https://www.centinelabible.org'];
+let corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.use('/events', eventsRoutes);
-
 const port = process.env.PORT || 3005;
 
 app.get('/', (req, res) => {
